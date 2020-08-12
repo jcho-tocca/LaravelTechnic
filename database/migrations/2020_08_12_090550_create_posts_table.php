b36679f9342d604cc671addbf11b226e6b93e046
jcho-tocca <jaeman.cho@systems.tocca-net.jp>
2020-08-12 18:18:55 +0900

    edit migration for users and posts

--- database/migrations/2020_08_12_090550_create_posts_table.php
+++ database/migrations/2020_08_12_090550_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->longText('contents');
             $table->timestamps();
         });
     }
