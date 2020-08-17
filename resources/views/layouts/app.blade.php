632dc9e4ec4b3fff771570b4e6fef8be978d5ee3
jcho-tocca <jaeman.cho@systems.tocca-net.jp>
2020-08-17 16:55:25 +0900

    fix "Uncaught ReferenceError: $ is not defined"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -81,5 +81,6 @@
             @yield('content')
         </main>
     </div>
+    @yield('script')
 </body>
 </html>
